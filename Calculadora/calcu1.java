92b3df2eeb5503f60e7feec64bce959ab6f0500c
armandoreal21 <Alumno@10.0.2.15>
2016-04-04 13:23:09 +0200

    creacion clase calculadora

---
+++ Calculadora/calcu1.java
@@ -0,0 +1,10 @@
+
+public class calcu1 {
+	int num1=0;
+	int num2=0;
+	Scanner sc=new Scanner(System.in);
+	
+	System.out.println("Introduce numero 1 ");
+	System.out.println("Introduce numero 2 ");
+
+}
